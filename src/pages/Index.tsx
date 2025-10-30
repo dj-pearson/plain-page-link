79893423f66d1d593aadcd634bec7694b42bb6f4
Dj Pearson <pearson.performance@gmail.com>
2025-10-29 23:34:50 -0500

    Revamp

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-background">
-      <h1 className="text-foreground text-base font-normal">link in bio</h1>
-    </main>
-  );
-};
-
-export default Index;
